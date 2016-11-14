e8cd24585e3b5017418d1ae5299c093d06bd66a7
Anton <arspex@gmail.com>
2016-11-14 23:21:15 +0200

    install flip/whoops error handler

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,25 @@
 <?php declare(strict_types=1);
 
-echo 'Hello World';
+namespace Arspex;
+
+use Whoops\Handler\PrettyPageHandler;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$config = require('config.php');
+
+/**
+ * Register the error handler
+ */
+$whoops = new \Whoops\Run();
+if ($config['debug']) {
+    $whoops->pushHandler(new PrettyPageHandler());
+} else {
+    /* TODO: Make something more meaningful */
+    $whoops->pushHandler(function($e) {
+        echo "Whoops!";
+    });
+}
+$whoops->register();
+
+throw new \Exception("Some exception");
